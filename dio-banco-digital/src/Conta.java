dcd3edcd757c1fe107cd5d4408a5741f1ad0b9eb
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-08 17:00:17 -0300

    Conceitos de Herança

---
+++ dio-banco-digital/src/Conta.java
@@ -0,0 +1,37 @@
+
+public class Conta implements IConta{
+	private int agencia;
+	private int numero;
+	private double saldo;
+	
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(double valor, Conta contaDestino) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+}
--- dio-banco-digital/src/ContaPoupanca.java
+++ dio-banco-digital/src/ContaPoupanca.java
@@ -1,30 +1,4 @@
 
-public class ContaPoupanca {
-	private int agencia;
-	private int numero;
-	private double saldo;
+public class ContaPoupanca extends Conta {
 	
-	public void sacar() {
-		
-	}
-	
-	public void depositar() {
-		
-	}
-	
-	public void transferir() {
-		
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}		
 }
