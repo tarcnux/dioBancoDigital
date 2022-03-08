a8f5f519e81e95badfb04033d717b9523ccd9b24
Tarcisio Nunes (tarcnux) <tarcisio.junior@zensa.com.br>
2022-03-08 16:50:03 -0300

    Conceitos de Abstração e Encapsulamento

---
+++ dio-banco-digital/src/ContaCorrente.java
@@ -0,0 +1,33 @@
+
+public class ContaCorrente {
+	
+	private int agencia;
+	private int numero;
+	private double saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
+}
